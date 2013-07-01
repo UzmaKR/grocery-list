88567daea7f5543b77a6612f8718a48997a24b3e
Apprentice <apprentice@devbootcamp.com>
2013-07-01 12:00:15 -0700

    grocery list initial commit

---
+++ application.js
@@ -0,0 +1,12 @@
+/* 
+ * What are the objects in this exercise?
+ * What are their properties and methods?
+ * How do your objects interact with their respective DOM elements?
+ */
+$(document).ready(function() {
+$('#store_list .item').draggable("enable");
+
+
+
+ });
+
